47f2a57d42ed0b70a026f78cb829133195566355
Ajay Sharma <101696598+asharma1616@users.noreply.github.com>
2025-05-05 11:47:40 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5556;
+
+export const mongoDBURL =
+  'mongodb+srv://ajaysharmav56:f6ZjyZsDktMnrVZE@cluster0.zo4urdl.mongodb.net/';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
